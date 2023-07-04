0c7e3e0a4084e0d07cee1bf4356abbd60ca250cb
jonhouseman <dofuswcar@gmail.com>
2023-07-04 15:29:20 -0600

    Starting project

---
+++ User.java
@@ -0,0 +1,14 @@
+package project;
+
+public class User{
+
+  private String name;
+  private int age;
+  private String info;
+
+  public User(String name, int age, String info){
+    this.name = name;
+    this.age = age;
+    this.info = info;
+  }
+}
---
+++ funcion_suma.js
@@ -0,0 +1,14 @@
+// Definición de una variable
+let nombre = "Juan";
+
+// Imprimir en la consola
+console.log("Hola, " + nombre + "!");
+
+// Función que suma dos números
+function sumar(a, b) {
+  return a + b;
+}
+
+// Uso de la función
+let resultado = sumar(5, 3);
+console.log("El resultado es: " + resultado);
