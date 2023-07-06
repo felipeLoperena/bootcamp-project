343eb073b03a09879d930588dd5074824cfbc70c
jonhouseman <dofuswcar@gmail.com>
2023-07-05 19:28:24 -0600

    Se agrego nueva variable long id

--- User.java
+++ User.java
@@ -2,11 +2,13 @@ package project;
 
 public class User{
 
+  private long id;
   private String name;
   private int age;
   private String info;
 
-  public User(String name, int age, String info){
+  public User(long id, String name, int age, String info){
+    this.id = id;
     this.name = name;
     this.age = age;
     this.info = info;
