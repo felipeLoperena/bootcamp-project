8049da3d5a0b588bf08f7e2fba5d4482a13a2046
Jonhouseman <75334258+felipeLoperena@users.noreply.github.com>
2023-07-06 19:03:46 -0600

    Update UserController.java

--- UserController.java
+++ UserController.java
@@ -3,6 +3,8 @@ import project.User;
 
 public class UserController{
 
+  //Revisar bien el constructor en un IDE para debuggearlo y saber que funciona bien
+
   public UserController(List<User> userlist){
 
     User user = new User(1, "Luis", 26, "Juega Minecraft");
